d8f177db55d36f3eb8456f3808766cc5e655b3de
Kyle Chen <kyle.h.chen@gmail.com>
2020-04-20 00:53:37 -0700

    Add styles, NavBar, Create page

--- pages/create.vue
+++ pages/create.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Events</h1>
+    <h1>Create an Event</h1>
   </div>
 </template>
 
@@ -8,4 +8,4 @@
 export default {}
 </script>
 
-<style></style>
+<style scoped></style>
